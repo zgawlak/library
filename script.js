1be8c18c25e625cb31944f462e076927aec04859
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-10 17:41:31 -0400

    Add display book card function, add basic card styles

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ function addBookToLibrary() {
     newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
+
+function displayBookCard() {
+    let bookCards = document.querySelector('.book-cards');
+    for (const book in myLibrary) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `Title: ${myLibrary[book].title}<br>Author: ${myLibrary[book].author}<br># of pages: ${myLibrary[book].pages}<br>${myLibrary[book].read}`;
+        bookCards.appendChild(card);
+    }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 444, 'yes');
+const sorcerersStone = new Book('Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', 234, 'no');
+const lifeOfPi = new Book('The Life of Pi', 'Yann Martel', 342, 'no');
+myLibrary.push(theHobbit);
+myLibrary.push(sorcerersStone);
+myLibrary.push(lifeOfPi);
+
+displayBookCard();
