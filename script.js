48fbb9ac1016b3ddc40f152dd4eb03d7999616b6
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-10 17:22:55 -0400

    Change read variable, create function to add book to library

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    if (read === 'yes') this.read = 'read';
+    else this.read = 'not read yet';
     this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     }
+}
+
+function addBookToLibrary() {
+    let title = window.prompt('Enter book title');
+    let author = window.prompt('Enter book\'s author');
+    let pages = window.prompt('Enter number of pages');
+    let read = window.prompt('Have you read the book?');
+    newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
